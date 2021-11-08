36bfb9ffd23b8133eb194e52989c03f55adc0db9
Vincent Laquindanum <laq.vince@gmail.com>
2021-11-08 15:28:44 -0800

    add register functionality

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -6,18 +6,21 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 import FormContainer from '../components/FormContainer'
 
-import { login } from '../actions/userActions'
+import { register } from '../actions/userActions'
 
-function LoginScreen({ location, history }) {
+function RegisterScreen({ location, history }) {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
     
     const dispatch = useDispatch()
 
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { error, loading, userInfo } = userLogin
+    const userRegister = useSelector(state => state.userRegister)
+    const { error, loading, userInfo } = userRegister
     
     useEffect(() => {
         if (userInfo) {
@@ -27,20 +30,39 @@ function LoginScreen({ location, history }) {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        dispatch(login(email, password))
+
+        if (password !== confirmPassword) {
+            setMessage('Passwords not match')
+        } else {
+            dispatch(register(name, email, password))
+        }
     }
 
 
     return (
         <FormContainer>
-            <h1>Sign In</h1>
+            <h1>Register</h1>
+            {message && <Message variant='danger'>{message}</Message>}
             {error && <Message variant='danger'>{error}</Message>}
             {loading && <Loader />}
             <Form onSubmit={submitHandler}>
 
+                <Form.Group controlId='name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        required
+                        type='name'
+                        placeholder='Enter Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
                 <Form.Group controlId='email'>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control
+                        required
                         type='email'
                         placeholder='Enter Email'
                         value={email}
@@ -52,6 +74,7 @@ function LoginScreen({ location, history }) {
                 <Form.Group controlId='password'>
                     <Form.Label>Password</Form.Label>
                     <Form.Control
+                        required
                         type='password'
                         placeholder='Enter Password'
                         value={password}
@@ -60,18 +83,30 @@ function LoginScreen({ location, history }) {
                     </Form.Control>
                 </Form.Group>
 
+                <Form.Group controlId='passwordConfirm'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Confirm Password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
                 <Button type='submit' variant='primary'>
-                    Sign In
+                    Register
                 </Button>
             </Form>
 
             <Row className='py-3'>
                 <Col>
-                    New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                    Have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Sign in</Link>
                 </Col>
             </Row>
+
         </FormContainer>
     )
 }
 
-export default LoginScreen
+export default RegisterScreen

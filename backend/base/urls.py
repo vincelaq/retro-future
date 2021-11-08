b84fc32484678b05c7660933053b6efe314fba8f
Vincent Laquindanum <laq.vince@gmail.com>
2021-11-08 10:22:54 -0800

    set up jwt token auth for backend

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    # User routes
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.getUserProfile, name="users-profile"),
+    path('users/', views.getUsers, name="users"),
+    # Product routes
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>/', views.getProduct, name="product"),
 ]

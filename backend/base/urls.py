6f1c592ce770b2ed89e78c9031d32ccdcee87bb9
Vincent Laquindanum <laq.vince@gmail.com>
2021-11-08 10:40:25 -0800

    add register users functionality

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
     # User routes
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
     path('users/profile/', views.getUserProfile, name="users-profile"),
     path('users/', views.getUsers, name="users"),
     # Product routes

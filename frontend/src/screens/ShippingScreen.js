24b601ad8f55895cd6b593609eafc6cdc082b79c
Vincent Laquindanum <laq.vince@gmail.com>
2021-11-09 08:29:16 -0800

    set up payment shipping page and checkout process

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import Checkout from '../components/Checkout'
 import { saveShippingAddress } from '../actions/cartActions'
 
 function ShippingScreen({ history }) {
@@ -24,6 +25,7 @@ function ShippingScreen({ history }) {
 
     return (
         <FormContainer>
+            <Checkout step1 step2/>
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
 

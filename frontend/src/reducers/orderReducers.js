c201c4cdb6bb6b399e40656eb2a181483a1bf6c1
Vincent Laquindanum <laq.vince@gmail.com>
2021-11-09 17:11:03 -0800

    add reducer and action for paying order

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -9,6 +9,11 @@ import {
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAIL,
 
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_RESET,
+
  } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state ={}, action) => {
@@ -62,4 +67,31 @@ export const orderDetailsReducer = (state ={loading: true, orderItems: [], shipp
         default:
             return state
     }
- }
+}
+
+export const orderPayReducer = (state ={}, action) => {
+    switch (action.type) {
+        case ORDER_PAY_REQUEST:
+            return {
+                loading: true
+            }
+
+        case ORDER_PAY_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+            }
+        
+        case ORDER_PAY_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        
+        case ORDER_PAY_RESET:
+            return {}
+
+        default:
+            return state
+    }
+}

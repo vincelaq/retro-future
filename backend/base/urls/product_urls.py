7cea642bbd605ec58d69984d76a585b9700aefbb
Vincent Laquindanum <laq.vince@gmail.com>
2021-11-09 21:16:11 -0800

    create backend review functionality

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,5 +5,6 @@ from base.views import product_views as views
 urlpatterns = [
     # Product routes
     path('', views.getProducts, name="products"),
+    path('<str:pk>/reviews/', views.createProductReview, name="create-review"),
     path('<str:pk>/', views.getProduct, name="product"),
 ]
